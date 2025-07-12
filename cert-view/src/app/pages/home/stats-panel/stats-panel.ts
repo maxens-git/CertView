5e2014618cc072ae4e1822ef6f598e0ed0241386
Maxens Verron <maxens@MacBook-Air-de-Maxens.local>
2025-07-12 16:46:51 +0200

    bind panel informations

--- cert-view/src/app/pages/home/stats-panel/stats-panel.ts
+++ cert-view/src/app/pages/home/stats-panel/stats-panel.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { PanelModule } from 'primeng/panel';
+import { CertificatService } from '../../../shared/services/CertificatesService';
+import { Certificate } from '../../../shared/interfaces/certificats.model';
 
 @Component({
   selector: 'app-stats-panel',
@@ -12,5 +14,30 @@ import { PanelModule } from 'primeng/panel';
   styleUrl: './stats-panel.scss'
 })
 export class StatsPanel {
-  certificatCount: number = 3;
+
+  certificateCount: number = 0;
+  validCertificateCount: number = 0;
+  expiredCertificateCount: number = 0;
+
+  protected certificats: Certificate[] = [];
+
+  constructor(private certificatService: CertificatService) {}
+
+  ngOnInit(): void {
+    this.loadCertificats();
+    this.updateInformations();
+  }
+
+  protected loadCertificats() {
+    this.certificatService.getCertificates().subscribe({
+      next: (certs) => this.certificats = certs,
+      error: () => console.error('Failed to load certificates')
+    });
+  }
+
+  protected updateInformations() {
+    this.certificateCount = this.certificats.length;
+    this.validCertificateCount = this.certificats.filter(c => c.valid).length;
+    this.expiredCertificateCount = this.certificateCount - this.validCertificateCount;
+  }
 }
